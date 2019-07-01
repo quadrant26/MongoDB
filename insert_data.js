307a07771559a469f19fabba674409e11a284e31
quadrant26 <quadrant26@163.com>
2019-07-01 18:26:13 +0800

    insert data demo 06

---
+++ insert_data.js
@@ -0,0 +1,42 @@
+var workmate1 = {
+    name: 'kang',
+    age: '30',
+    sex: 1,
+    job: '前端',
+    skill: {
+        skillOne: 'HTML+CSS',
+        skillTwo: 'javascript',
+        skillThree: 'python'
+    },
+    regeditTime: Date.now()
+}
+var workmate2 = {
+    name: 'lilei',
+    age: '40',
+    sex: 1,
+    job: '后台',
+    skill: {
+        skillOne: 'java',
+        skillTwo: 'J2EE',
+        skillThree: 'PPT'
+    },
+    regeditTime: Date.now()
+}
+var workmate3 = {
+    name: 'hanmeimei',
+    age: '26',
+    sex: 0,
+    job: 'UI',
+    skill: {
+        skillOne: 'photoshop',
+        skillTwo: 'AI',
+        skillThree: 'PPT'
+    },
+    regeditTime: Date.now()
+}
+
+var db = connect('company');
+var workmateArrar = [workmate1, workmate2, workmate3];
+db.workmate.insert(workmateArrar)
+
+print("[SUCCESS] The data was inserted successfully!")
