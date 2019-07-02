e4ba95b34303147958074c63dbcb4553649eb146
quadrant26 <quadrant26@163.com>
2019-07-02 18:07:52 +0800

    符合索引 hint 指定索引 dropIndex 删除索引

--- mongo_index_start.js
+++ mongo_index_start.js
@@ -1,12 +1,25 @@
 var startTime = Date.now();
 
 var db = connect("company")
-var rs = db.randomInfo.find({userName: '7xw8y3'});
-
-rs.forEach( rs => {printjson(rs)})
+// var rs = db.randomInfo.find({userName: '7xw8y3'});
+// rs.forEach( rs => {printjson(rs)})
 
 // 查看索引 db.randomInfo.getIndexes()
 // 建立索引 db.randomInfo.ensureIndex({field: 1});
+// db.randomInfo.ensureIndex({userName: 1})
+// 复合索引 建立两条以上的索引
+// db.randomInfo.ensureIndex({randNum0: 1})
+
+// var rs = db.randomInfo.find({userName: '7xw8y3'});
+// rs.forEach( rs => {printjson(rs)})
+
+// 指定索引查询（hint）
+var rs = db.randomInfo.find({userName:'7xwb8y3',randNum0:565509}).hint({randNum0:1});
+rs.forEach( rs => {printjson(rs)})
+
+// 删除索引
+// randNum0_1 是getIndexes 返回的 name 值， 不是 key 值
+// db.randomInfo.dropIndex('randNum0_1');//索引的唯一ID
 
 var endTime = Date.now() - startTime;
 print("[demo]: endTime=" + endTime + 'ms')
